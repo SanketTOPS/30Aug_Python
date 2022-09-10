66b2871d3b1dc517652dc51a425d1a9607c2359b
Django-TOPS <djangotestmail2021@gmail.com>
2022-09-10 09:20:05 +0530

    loops

---
+++ Module-1/pattern1.py
@@ -0,0 +1,12 @@
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+"""
+
+for r in range(1,6): #root
+    for c in range(r):
+        print("*",end=" ")
+    print(" ")
---
+++ Module-1/pattern4.py
@@ -0,0 +1,8 @@
+k=0
+for r in range(1,7):
+    for c in range(1,7):
+        print("*",end=" ")
+        k+=1
+        if k==2:
+            print(" ",end=" ")
+    print(" ")
