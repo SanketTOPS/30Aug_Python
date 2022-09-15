1d2ec34c93cf12e88babf2c584622e8784e47c57
Django-TOPS <djangotestmail2021@gmail.com>
2022-09-15 09:19:58 +0530

    List

---
+++ Module-2_Collection/list_example.py
@@ -0,0 +1,59 @@
+city=['rajkot','ahmedabad','surat','baroda']
+"""print(city)
+print(city[0])
+print(city[-1])
+print(city[0:3])
+print(city[2:])
+print(city[:2])"""
+
+#print(len(city))
+
+#print(city)
+#city[0]='bhavnagar'
+#print(city)
+
+# --------------------------------- #
+
+"""if 'rajkot' in city:
+    print("Yes...")
+else:
+    print("Noo")"""
+
+"""for i in city:
+    print(i)"""
+
+#Index[0] = rajkot
+#Index[1] = ahmedabad
+
+#print(city.index('baroda'))
+"""for i in city:
+    print(f"Index[{city.index(i)}] = {i}")"""
+
+# ------------------------------------------------------ #
+
+#print(city)
+city.append('bhavnagar')
+#city.insert(2,'wadhwan')
+#city.pop()
+#city.pop(2)
+#city.remove('rajkot')
+#city.clear()
+#del city
+#del city[1]
+#city.sort()
+#city.reverse()
+#print(city)
+
+#newlist=city.copy()
+#print(newlist)
+
+newlist=['A','B','C']
+print(city)
+print(newlist)
+#print(city+newlist)
+newlist.extend(city)
+print(newlist)
+
+
+
+
